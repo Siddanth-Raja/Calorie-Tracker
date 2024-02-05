6210305c4b7a548428c168a0c112fe15c4e930c3
sasddu0 <142539009+sasddu0@users.noreply.github.com>
2024-02-05 14:14:39 -0600

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Siddanth Raja on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
